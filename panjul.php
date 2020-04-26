c1016ead5e8c561d959bc127b74a84b0388240fa
Conan Edogawa Kun <64359092+conanedogawakun@users.noreply.github.com>
2020-04-26 19:19:45 +0700

    Update panjul.php

--- panjul.php
+++ panjul.php
@@ -8,11 +8,11 @@ include 'tri_req.php';
 
 $tri = new tri();
 $imei = "868880043302499";
-echo "Masukkan No Telepon : ";
+echo "Masukkan No Telepon Ya Kontol : ";
 $msisdn = trim(fgets(STDIN));
 $otp = $tri->request_otp($msisdn,$imei);
 echo $otp[1] . "\r\n";
-echo "Masukkan OTP : ";
+echo "Masukkan OTP Kontol : ";
 $otp = trim(fgets(STDIN));
 $login = $tri->login($msisdn,$otp);
 $login = json_decode($login,true);
